f8281c796e36d4417bd6c00ac95ac01d2942b96b
AlexMainaMunyua <alexmaina79@gmail.com>
2021-12-12 14:45:47 +0300

    feat: Special rich text function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_testing_application/src/creamy_field.dart';
-import 'package:flutter_testing_application/src/pay.dart';
-import 'package:flutter_testing_application/src/search_and_highlight.dart';
-import 'package:google_pay/google_pay.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,13 +23,82 @@ class MyApp extends StatelessWidget {
       // creamy field
       // home: MyEditorApp()
 
-      home: PayMaterialApp(),
+      // home: MyhomePage(),
 
       // home: WelcomeToGroup(),
 
-      // home: MergeTextStyle(),
+      home: ChangeFontStyle(),
     );
   }
 }
 
+class MyhomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyhomePage> {
+  static const platform =
+      const MethodChannel('flutter_testing_application/launch_contact');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contact Simple"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Launch contact activity"),
+          onPressed: () async => launchContact(),
+        ),
+      ),
+    );
+  }
+
+  launchContact() async {
+    try {
+      await platform.invokeMethod('launch');
+    } on PlatformException catch (e) {
+      print("Failed to launch contact: ${e.message}");
+    }
+  }
+}
+
+class ChangeFontStyle extends StatelessWidget {
+
+
+  TextSpan rich(String input) {
+    final styles = {
+      '_': const TextStyle(fontStyle: FontStyle.italic),
+      '*': const TextStyle(fontWeight: FontWeight.bold),
+      '~': const TextStyle(decoration: TextDecoration.lineThrough),
+    };
+    final spans = <TextSpan>[];
+    input.trim().splitMapJoin(RegExp(r'([_*~])(.*)?\1'), onMatch: (m) {
+      spans.add(TextSpan(text: m.group(2), style: styles[m.group(1)]));
+      return '';
+    }, onNonMatch: (String text) {
+      spans.add(TextSpan(text: text));
+      return '';
+    });
+    return TextSpan(style: const TextStyle(fontSize: 24), children: spans);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Smart Text"),),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            Text("Hello"),
+            RichText(text: rich('_Hello_')),
+            Text("Hello")
+          ],
+        ),
+      ),
+    );
+  }
+}
