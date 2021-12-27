f7fe17b2372bbc2f77e363d1bc5d6abbf32e6cc9
AlexMainaMunyua <alexmaina79@gmail.com>
2021-12-27 13:35:52 +0300

    Feat: recaptcha

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recaptcha_v2/flutter_recaptcha_v2.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
 
 void main() => runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       ),
 
       // search and Highlights
-      home: HighLight()
+      // home: HighLight()
 
       // contact Service
       // home: ContactsExampleApp(),
@@ -28,6 +29,66 @@ class MyApp extends StatelessWidget {
       // home: WelcomeToGroup(),
 
       // home: ChangeFontStyle(),
+      home: MyHomePage(title:  ' Google ReCaptcha Demo',)
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String verifyResult = "";
+
+  RecaptchaV2Controller recaptchaV2Controller = RecaptchaV2Controller();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("SHOW ReCAPTCHA"),
+                  onPressed: () {
+                    recaptchaV2Controller.show() ;
+                  },
+                ),
+                Text(verifyResult),
+              ],
+            ),
+          ),
+          RecaptchaV2(
+            apiKey: "6LeCwZYUAAAAAJo8IVvGX9dH65Rw89vxaxErCeou",
+            apiSecret: "6LeCwZYUAAAAAKGahIjwfOARevvRETgvwhPMKCs_",
+            controller: recaptchaV2Controller,
+            onVerifiedError: (err){
+              print(err);
+            },
+            onVerifiedSuccessfully: (success) {
+              setState(() {
+                if (success) {
+                  verifyResult = "You've been verified successfully.";
+                } else {
+                  verifyResult = "Failed to verify.";
+                }
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
