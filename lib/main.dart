c28aae5f9315d5b8740edd4fd6074ec01c25529a
AlexMainaMunyua <alexmaina79@gmail.com>
2022-01-11 09:29:45 +0300

    StakeOverflow question

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,34 @@ class MyApp extends StatelessWidget {
       // home: WelcomeToGroup(),
 
       // home: ChangeFontStyle(),
-      home: MyHomePage(title:  ' Google ReCaptcha Demo',)
+      // home: MyHomePage(title:  ' Google ReCaptcha Demo',)
+      home: RoundButton(),
+    );
+  }
+}
+
+
+class RoundButton extends StatelessWidget {
+  const RoundButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("App Bar"),),
+      body: Center(
+        child: Container(
+    height: 100,
+    width: 100,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(40),
+    ),
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 15)),
+      onPressed: (){},
+      child: const Text('My basket'),
+    ),
+  ), 
+      ),
     );
   }
 }
