2d439e4f43fcfbf11e15e8bf882828b7f2263d34
AlexMainaMunyua <alexmaina79@gmail.com>
2022-06-22 16:25:41 +0300

    refactor: rename file and create new files

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_recaptcha_v2/flutter_recaptcha_v2.dart';
-import 'package:flutter_testing_application/src/in_app_billing.dart';
-import 'package:flutter_testing_application/src/in_app_purchases.dart';
-import 'package:flutter_testing_application/src/pay.dart';
-import 'package:flutter_testing_application/src/stackoverflow/button.dart';
-import 'package:scroll_to_index/scroll_to_index.dart';
 
 void main() => runApp(MyApp());
 
@@ -34,53 +28,17 @@ class MyApp extends StatelessWidget {
       // home: WelcomeToGroup(),
 
       // home: ChangeFontStyle(),
+
       // home: MyHomePage(title:  ' Google ReCaptcha Demo',)
-      home: MyAppChachedNetworkImage(),
-    );
-  }
-}
 
-
-class MyAppChachedNetworkImage extends StatelessWidget {
-  const MyAppChachedNetworkImage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    const title = 'Cached Images';
-
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(title),
-        ),
-        body: Center(
-          child: CachedNetworkImage(
-            placeholder: (context, url) => const CircularProgressIndicator(),
-            imageUrl: 'https://picsum.photos/250?image=9',
-          ),
-        ),
-      ),
+      // home: TabBarDemo(),
+      // home: MyAppBilling(),
     );
   }
 }
 
 
 
-// class CircleAva extends StatelessWidget {
-//   const CircleAva({Key key}) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         child: Center(
-//           child: CircleAvatar(
-//             radius: 60,
-//             backgroundColor: Colors.blue,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+
+
--- lib/packages/in_app_billing.dart
+++ lib/packages/in_app_billing.dart
@@ -23,7 +23,7 @@ class _MyAppBillingState extends State<MyAppBilling> {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-    await FlutterInAppBilling.initBp(
+      await FlutterInAppBilling.initBp(
           "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxS0EbQG9LEvv+9Oik03+WdjQ2InH+ykybDGI2hyo13cCI+LC0wAW/00Xh9g6yVzqXZCAjQ9dFVln4UQNT7XoOvUnQM4xSYv2rl62rGUzUlmiYs+e0docAehnuM30WhmqSHlg5eMVv5sr3B6qlYq7wCtZ4p7Eqyn+8rbl2hJ1EcvK1d8gedcum/GS7uF2eLhU4SBGWTFkRNuqUaaUpTO+4ONu9Zv2r8qn94h5fXlg9cyXX3tABkKBCLtAHdtgnHVVWQazLeM960sIVV9928Z4JKWCtXIjfTPa27Y+an+Uq5ilBYF6dgW5kVZk6bfe/eo22r7VC8HHdRJKPrGpHBplZwIDAQAB");
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
@@ -53,7 +53,8 @@ class _MyAppBillingState extends State<MyAppBilling> {
             ),
             FlatButton(
                 onPressed: () {
-                  FlutterInAppBilling.subscribe("test");
+                  FlutterInAppBilling.subscribe(
+                      "0018ece943ac4991a36b2a67c8a6764f");
                 },
                 child: Text("Buy"))
           ],
